bd61e258e332637c3d223eb731f131d62fed234d
bernhard <Bernhard.Schmalhofer@gmx.de>
2021-02-04 21:37:10 +0100

    Issue #5: cleanup
    Remove OTOBO policy ProhibitUnless, as postfix unless is allowed now
    Use a state variable for caching.
    Remove an obsolete check for the framework version.
    Do not remove the 'use v5.24;' lines when doing the syntax check.

--- Kernel/TidyAll/Plugin/OTOBO/Perl/PerlCritic.pm
+++ Kernel/TidyAll/Plugin/OTOBO/Perl/PerlCritic.pm
@@ -2,7 +2,7 @@
 # OTOBO is a web-based ticketing system for service organisations.
 # --
 # Copyright (C) 2001-2020 OTRS AG, https://otrs.com/
-# Copyright (C) 2019-2020 Rother OSS GmbH, https://otobo.de/
+# Copyright (C) 2019-2021 Rother OSS GmbH, https://otobo.de/
 # --
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
@@ -18,30 +18,31 @@ package TidyAll::Plugin::OTOBO::Perl::PerlCritic;
 
 use strict;
 use warnings;
+use v5.24;
+use utf8;
 
 use File::Basename;
 use lib dirname(__FILE__) . '/../';    # Find our Perl::Critic policies
 
 use parent qw(TidyAll::Plugin::OTOBO::Perl);
-use Perl::Critic;
 
+use Perl::Critic;
 use Perl::Critic::Policy::OTOBO::ProhibitGoto;
 use Perl::Critic::Policy::OTOBO::ProhibitLowPrecendeceOps;
 use Perl::Critic::Policy::OTOBO::ProhibitSmartMatchOperator;
 use Perl::Critic::Policy::OTOBO::ProhibitRandInTests;
 use Perl::Critic::Policy::OTOBO::ProhibitOpen;
-#use Perl::Critic::Policy::OTOBO::ProhibitUnless;
 use Perl::Critic::Policy::OTOBO::RequireCamelCase;
 use Perl::Critic::Policy::OTOBO::RequireLabels;
 use Perl::Critic::Policy::OTOBO::RequireParensWithMethods;
 use Perl::Critic::Policy::OTOBO::RequireTrueReturnValueForModules;
 
-# Cache Perl::Critic object instance to save time. But cache it
-#   for every framework version, because the configuration may differ.
-our $CachedPerlCritic = {};
-
 sub validate_file {
     my ( $Self, $Filename ) = @_;
+
+    # Cache Perl::Critic object instance to save time. But cache it
+    # for every framework version, because the configuration may differ.
+    state $CachedPerlCritic = {};
 
     return if $Self->IsPluginDisabled( Filename => $Filename );
     return if $Self->IsFrameworkVersionLessThan( 3, 2 );
@@ -50,22 +51,21 @@ sub validate_file {
 
     if ( !$CachedPerlCritic->{$FrameworkVersion} ) {
 
-        my $Severity = 4;    # STERN
-        if ( $Self->IsFrameworkVersionLessThan( 6, 0 ) ) {
-            $Severity = 5;    #  GENTLE, less strict for older versions
-        }
+        my $Severity = 4;    # STERN, only $SEVERITY_HIGHEST = 5 and $SEVERITY_HIGH = 4 are covered
+
         my $Critic = Perl::Critic->new(
             -severity => $Severity,
             -exclude  => [
                 'Modules::RequireExplicitPackage',    # this breaks in our scripts/test folder
             ],
         );
+
+        # explicitly add the OTOBO policies, run them regardless of severity
         $Critic->add_policy( -policy => 'OTOBO::ProhibitGoto' );
         $Critic->add_policy( -policy => 'OTOBO::ProhibitLowPrecendeceOps' );
         $Critic->add_policy( -policy => 'OTOBO::ProhibitOpen' );
         $Critic->add_policy( -policy => 'OTOBO::ProhibitRandInTests' );
         $Critic->add_policy( -policy => 'OTOBO::ProhibitSmartMatchOperator' );
-#        $Critic->add_policy( -policy => 'OTOBO::ProhibitUnless' );
         $Critic->add_policy( -policy => 'OTOBO::RequireCamelCase' );
         $Critic->add_policy( -policy => 'OTOBO::RequireLabels' );
         $Critic->add_policy( -policy => 'OTOBO::RequireParensWithMethods' );
