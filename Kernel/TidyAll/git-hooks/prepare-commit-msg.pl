8586a9c8a3acf8881117726c22af875104e4574f
bernhard <Bernhard.Schmalhofer@gmx.de>
2021-02-15 17:42:48 +0100

    Issue #8: also generate a reference to the relevant issue
    when the branch is named accordingly

--- Kernel/TidyAll/git-hooks/prepare-commit-msg.pl
+++ Kernel/TidyAll/git-hooks/prepare-commit-msg.pl
@@ -18,31 +18,88 @@
 use strict;
 use warnings;
 
+# core modules
 use IO::File;
 
-=head1 SYNOPSIS
+# CPAN modules
 
-This hook inserts a custom prepared commit message into the git commit message.
+# OTOBO modules
+
+=head1 NAME
+
+prepare-commit-msg.pl - a git hook
+
+=head1 DESCRIPTION
+
+This git hook preset the commit message when running I<git commit>.
+Two different features are supported.
+
+=head2 reference the Github issue based on a naming convention for the branch
+
+When the current git branch is recognised as an issue branch then
+the commit message is prepended with 'Issue #<ISSUE_ID>:'.
+
+Examples for issue branch names are:
+
+    issue-#513-prepare_commit_msg
+    issue-rotheross/otobo#513-prepare_commit_msg
+    issue-RotherOSS/otobo-docker#1-example
+    issue-RotherOSS/otobo-docker#2-x
+
+
+=head2 include the file .git/OTOBOCommitTemplateFile.msg
+
+This is support for a workflow that generates text for the commit message and writes it to
+an agreed upon file. After including it, the transfer file is deleted.
+
+Currently no example of such a workflow is known.
 
 =cut
 
-my $OTOBOCommitTemplateFile = '.git/OTOBOCommitTemplate.msg';
+# git calls this script with the arguments: COMMIT_MSG_FILE, COMMIT_SOURCE, SHA1.
+# Here only COMMIT_MSG_FILE and COMMIT_SOURCE are needed.
+my ( $GitCommitMsgFile, $GitCommitSource ) = @ARGV;
 
-if ( -r $OTOBOCommitTemplateFile ) {
+my @CustomCommitMsg;
 
-    # Get our content and prepend it
-    my $FileHandle = IO::File->new( $OTOBOCommitTemplateFile, 'r' );
-    my @Content    = $FileHandle->getlines();
+# feature 1
+# For now we mess only with interactive commits, that is when $GitCommitSource is empty.
+if ( !$GitCommitSource ) {
 
-    # Get the pre-populated file from GIT and keep its contents
-    my $GitCommitTemplateFile = shift;
-    $FileHandle = IO::File->new( $GitCommitTemplateFile, 'r' );
-    push @Content, $FileHandle->getlines();
+    # find out which branch we are on
+    my $BranchName = `git rev-parse --abbrev-ref HEAD`;
+    chomp $BranchName;
+
+    # Do nothing unless the issue can be extracted from the git branch name.
+    if ( my ($Issue) = $BranchName =~ m/^issue-(.*#[0-9]+)-[^-]+/ ) {
+
+        # Prepend the issue to the commit message.
+        push @CustomCommitMsg, "Issue $Issue:";
+    }
+}
+
+# feature 2
+{
+    my $OTOBOCommitTemplateFile = '.git/OTOBOCommitTemplate.msg';
+
+    if ( -r $OTOBOCommitTemplateFile ) {
+
+        # Get our content and prepend it
+        my $FileHandle = IO::File->new( $OTOBOCommitTemplateFile, 'r' );
+        push @CustomCommitMsg, $FileHandle->getlines();
+
+        # Remove custom commit message template
+        unlink $OTOBOCommitTemplateFile;
+    }
+}
+
+# prepend the custom text to the commit message
+if (@CustomCommitMsg) {
+
+    my $FileHandleIn = IO::File->new( $GitCommitMsgFile, 'r' );
+    my @OldCommitMsg = $FileHandleIn->getlines();
 
     # Write new commit message
-    $FileHandle = IO::File->new( $GitCommitTemplateFile, 'w' );
-    $FileHandle->print( join "", @Content );
-
-    # Remove custom commit message template
-    unlink $OTOBOCommitTemplateFile;
+    my $FileHandleOut = IO::File->new( $GitCommitMsgFile, 'w' );
+    $FileHandleOut->print( join '', @CustomCommitMsg, @OldCommitMsg );
 }
